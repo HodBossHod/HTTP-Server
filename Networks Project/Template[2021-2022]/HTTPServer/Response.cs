8f79b8d13542033ade9caad1a91f6e8340647685
Hani-SOEN <76478572+Hani-SOEN@users.noreply.github.com>
2021-12-28 12:20:32 +0200

    add_implemention_class_response

--- Networks Project/Template[2021-2022]/HTTPServer/Response.cs
+++ Networks Project/Template[2021-2022]/HTTPServer/Response.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
+using System.Threading.Tasks;
 
 namespace HTTPServer
 {
@@ -26,15 +27,43 @@ namespace HTTPServer
                 return responseString;
             }
         }
+
         StatusCode code;
+
         List<string> headerLines = new List<string>();
         public Response(StatusCode code, string contentType, string content, string redirectoinPath)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+
             // TODO: Add headlines (Content-Type, Content-Length,Date, [location if there is redirection])
 
+            headerLines.Add(contentType);
+            headerLines.Add(content.Length.ToString());
+            headerLines.Add(DateTime.Now.ToString("ddd, dd MMM yyy HH’:’mm’:’ss ‘EST’"));
+            string status = GetStatusLine(code);
 
-            // TODO: Create the request string
+
+            if (code == StatusCode.Redirect)
+            {
+                headerLines.Add(redirectoinPath);
+
+                // TODO: Create the request string
+
+                responseString = status + "\r\n"
+                    + "Content-Type :" + headerLines[0] + "\r\n"
+                    + "Content-length :" + headerLines[1] + "\r\n"
+                    + "Date :" + headerLines[2] + "\r\n"
+                    + "Location :" + headerLines[3] + "\r\n" + "\r\n"
+                    + content;
+            }
+            else
+            {
+                responseString = status + "\r\n"
+                       + "Content-Type :" + headerLines[0] + "\r\n"
+                       + "Content-length :" + headerLines[1] + "\r\n"
+                       + "Date :" + headerLines[2] + "\r\n" + "\r\n"
+                       + content;
+            }
 
         }
 
@@ -42,7 +71,7 @@ namespace HTTPServer
         {
             // TODO: Create the response status line and return it
             string statusLine = string.Empty;
-
+            statusLine += string.Format("HTTP/1.1 {0} {1}", ((int)code).ToString(), code.ToString());
             return statusLine;
         }
     }
